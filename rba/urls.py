7cd39a507e8895ce8ee0561edb79edba74fd38fb
Timo Lubitz <timo.lubitz@gmail.com>
2020-11-24 15:51:03 +0100

    init

---
+++ rba/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('simulator/', include('simulator.urls')),
+    path('', RedirectView.as_view(url='simulator/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ simulator/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    '''
+    '''
+
+    context = { }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
