97ef1047fe9e71d34f533ee78ca0ac1fe8a85bad
Timo Lubitz <timo.lubitz@gmail.com>
2020-11-27 13:59:10 +0100

    secret key env

--- rba/settings.py
+++ rba/settings.py
@@ -20,7 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5y!f&1s2-)eju@8j_)cj1b1wl7+c!-v%xb*1+q295t!57ul0^$'
+SECRET_KEY = os.getenv("SECRET_KEY")       #prod
+
+if SECRET_KEY == None:
+    dev = True
+    SECRET_KEY = '5y!f&1s2-)eju@8j_)cj1b1wl7+c!-v%xb*1+q295t!57ul0^$' #dev
+    print('*>>>>> You are working in DEVELOPMENT mode <<<<<*')
+else:
+    dev = False
+    print('*>>>>> You are working in PRODUCTION mode <<<<<*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
