1a793f693a4bb0db76f94ea6b8280216eb8aaaa1
Timo Lubitz <timo.lubitz@gmail.com>
2020-11-27 13:07:38 +0100

    readded rbastructure directory

---
+++ simulator/static/python/rbastructure/model_Data.py
@@ -0,0 +1,12 @@
+# python 2/3 compatibility
+from __future__ import division, print_function
+
+
+class ModelData(object):
+    """
+    Class holding information on the model.
+
+    NOT specified so far. Empty Parent.
+
+    """
+    pass
