92c314369a9b063acf6c3fc71b39bbc3c2a6922f
Timo Lubitz <timo.lubitz@gmail.com>
2020-12-08 13:38:23 +0100

    corrected JS paths

--- simulator/static/js/scripts_index.js
+++ simulator/static/js/scripts_index.js
@@ -1,14 +1,20 @@
+// determine dev/prod mode
+if (window.location.href.substr(0,10) === 'http://127') { var mode = 'dev'; }
+else { var mode = 'prod'; }
+
 function delete_session() {
     $.get('/simulator/clearsession', function(data) {
         console.log('Deleting session');
-        window.location.href="http://127.0.0.1:8000/simulator";
+        if (mode == 'dev') { window.location.href="http://127.0.0.1:8000/simulator"; }
+        else { window.location.href="https://timosan.pythonanywhere.com/simulator/"; }
     });
 }
 
 function simulate() {
     $.get('/simulator/simulate', function(data) {
         console.log('Simulating...');
-        window.location.href="http://127.0.0.1:8000/simulator";
+        if (mode == 'dev') { window.location.href="http://127.0.0.1:8000/simulator"; }
+        else { window.location.href="https://timosan.pythonanywhere.com/simulator/"; }
     });
 }
 
@@ -18,7 +24,8 @@ function load_model(modelname) {
     // send the modelname to the view
     $.post('/simulator/loadmodel/', JSON.stringify(model_name_dict), function(data) {
         console.log('Choosing model from DB');
-        window.location.href="http://127.0.0.1:8000/simulator";
+        if (mode == 'dev') { window.location.href="http://127.0.0.1:8000/simulator"; }
+        else { window.location.href="https://timosan.pythonanywhere.com/simulator/"; }
     });
 
 }
